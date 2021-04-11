cfbf6e522b18f26c89a4261f7e7e0c7594546a16
maescristro <Chrisdeparis@users.noreply.github.com>
2021-04-11 23:03:56 +0200

    date arrive component

--- src/components/DateArrivee.js
+++ src/components/DateArrivee.js
@@ -2,13 +2,58 @@ import 'date-fns';
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import DateFnsUtils from '@date-io/date-fns';
+import TextField from '@material-ui/core/TextField';
 import {
   MuiPickersUtilsProvider,
   KeyboardTimePicker,
   KeyboardDatePicker,
 } from '@material-ui/pickers';
+import {
+
+  makeStyles,
+
+} from '@material-ui/core/styles';
+
+
+import './DateArrivee.css';
+
+const useStylesReddit = makeStyles((theme) => ({
+  root: {
+    border: '1px solid #e2e2e1',
+    overflow: 'hidden',
+    borderRadius: 4,
+    backgroundColor: '#fcfcfb',
+    transition: theme.transitions.create(['border-color', 'box-shadow']),
+    '&:hover': {
+      backgroundColor: '#fff',
+    },
+    '&$focused': {
+      backgroundColor: '#fff',
+      border: '2px solid #000',
+    },
+  },
+  focused: {},
+}));
+
+
+function RedditTextField(props) {
+  const classes = useStylesReddit();
+
+  return <TextField InputProps={{ classes, disableUnderline: true }} {...props} />;
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  margin: {
+    margin: theme.spacing(1),
+  },
+}));
 
 export default function DateArrivee() {
+  const classes = useStyles();
   // The first commit of Material-UI
   const [selectedDate, setSelectedDate] = React.useState(new Date('2014-08-18T21:11:54'));
 
@@ -17,23 +62,36 @@ export default function DateArrivee() {
   };
 
   return (
-    <MuiPickersUtilsProvider utils={DateFnsUtils}>
-      <Grid>
-        <KeyboardDatePicker
-          disableToolbar
-          variant="inline"
-          format="MM/dd/yyyy"
-          margin="normal"
-          id="date-picker-inline"
-          label="ARRIVÉE"
-          value={selectedDate}
-          onChange={handleDateChange}
-          KeyboardButtonProps={{
-            'aria-label': 'change date',
-          }}
-        />
+    <>
+    <form className={classes.root} noValidate>   
+      <RedditTextField
+        label="ARRIVÉE"
+        className={classes.margin}
+        variant="filled"
+      >
         
-      </Grid>
-    </MuiPickersUtilsProvider>
+      
+      </RedditTextField>
+    </form>
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+          <Grid>
+            <KeyboardDatePicker
+              className={classes.margin}
+              disableToolbar
+              variant="inline"
+              format="MM/dd/yyyy"
+              margin="normal"
+              id="date-picker-inline"
+              label="ARRIVÉE"
+              value={selectedDate}
+              onChange={handleDateChange}
+              KeyboardButtonProps={{
+                'aria-label': 'change date',
+              }}
+            />        
+          </Grid>
+        </MuiPickersUtilsProvider>
+    
+    </>
   );
 }
