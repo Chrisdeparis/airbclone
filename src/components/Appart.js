497d13ad0847011dd7d239cdb7aea9e6b0384634
maescristro <milfortchristian@gmail.com>
2021-04-12 17:59:28 +0200

    AppartPhoto Comp

--- src/components/Appart.js
+++ src/components/Appart.js
@@ -9,11 +9,13 @@ import Box from '@material-ui/core/Box'
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { shadows } from '@material-ui/system';
-//import StarIcon from '@material-ui/icons/Star';
+import StarIcon from '@material-ui/icons/Star';
+import AppartPhoto from './AppartPhoto';
 
 const useStyles = makeStyles({
   root: {
     maximumWidth: 387,
+    
   },
   media: {
     height: 259,
@@ -21,6 +23,9 @@ const useStyles = makeStyles({
     border: 0,
     boxShadow:0,
   },
+  starIcon: {
+      marginTop: 15,
+  }
 });
 
 export default function MediaCard() {
@@ -34,7 +39,17 @@ export default function MediaCard() {
           image="https://a0.muscache.com/im/pictures/69556101/41a4a2be_original.jpg?im_w=480"
           title="Contemplative Reptile"
         />
-        les 5 étoiles
+        <StarIcon className="starIcon" />
+        <StarIcon className="starIcon" />
+        <StarIcon className="starIcon" />
+        <StarIcon className="starIcon" />
+        <StarIcon className="starIcon" />
+        <p className="appart__desc">Un endroit magique au coeur de Bordeaux…Tout était parfait…sauf la pluie…mais ça Antoine n'y pouvait rien ! 
+        Un appartement où il fait bon vivre et tout là…même plus…
+        Nous recommandons chaudement et reviendrons certainement.
+        Un grand merci à Antoine qui est un hôte ...
+        </p>
+        <AppartPhoto />
       </CardActionArea>
     </Card>
   );
---
+++ src/components/AppartPhoto.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AppartPhoto() {
+    return (
+        <div>
+            <div className="appart__photo"></div>
+        </div>
+    )
+}
+
+export default AppartPhoto
