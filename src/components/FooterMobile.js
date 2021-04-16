a5c2633987aba11a57e7148823e4d276407c9c40
maescristro <milfortchristian@gmail.com>
2021-04-16 18:12:57 +0200

    footer effect

--- src/components/FooterMobile.js
+++ src/components/FooterMobile.js
@@ -10,50 +10,11 @@ function FooterMobile() {
     const [footer, setFooter] = useState(false);
     
 
-    // const hideFooter = () => {
-    //     let scrollTop=0;
-    //     let lastScrollTop = 0;
-        
-    //     const footer = document.getElementById('footer');
-
-    //     window.addEventListener("scroll", function() {
-    //         let scrollTop = window.pageYOffset;
-    //     })
-
-    //     //  const EndPage = window.scrollY;
-    //     //  console.log('scroll = ' + window.scrollY);
-    //     //  if(window.scrollY < 80) {
-
-    //     //     console.log('on cache le footer');
-    //     //     console.log(scrollTop);
-    //     //     setFooter(true)
-    //     //     footer.style.bottom="-80px";
-
-    //     // } else {
-    //     //     footer.style.bottom="0";
-    //     //     setFooter(false);
-            
-    //     //     console.log('on montre le footer');
-    //     // }
-    //     // lastScrollTop = scrollTop;
-    // }
-
-    useEffect(() => {
-        // hideFooter();
-    }, []);
-
-    // window.addEventListener('scroll', hideFooter);  
-
-    // const changeBackground = () => {
-    //     console.log(window.scrollY);
-    //     if(window.scrollY > 80) {
-    //         setFooter(true);
-    //     } else {
-    //         setFooter(false);
-    //     }
-    // }
-
-    // window.addEventListener('scroll', changeBackground);
+    const hideFooter = () => {
+        //quand le scroll est en fin de page
+        //le footer disparait de l'écran.
+        const footer = document.getElementById('footer');
+    };
 
 
     return (
