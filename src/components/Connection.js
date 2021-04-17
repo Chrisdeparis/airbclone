ffb19ef899afe45caddc050a8dba83d7bcc9cfce
maescristro <milfortchristian@gmail.com>
2021-04-17 20:38:22 +0200

    toggle button

--- src/components/Connection.js
+++ src/components/Connection.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import MenuIcon from '@material-ui/icons/Menu';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import Popup from './Popup';
+
+
+
+
 function Connection() {
+    
+    const [show, setShow] = useState(false);
+
+    const openModal = () => setShow(true);
+
+    console.log(setShow);
+
+    useEffect(() => {
+        setShow();
+    }, []);
+
+    console.log(show);
+
     return (
         <div>
-            <button className="connect__btn"><MenuIcon /><AccountCircleIcon className="user" /></button>
+        <button className="connect__btn" onClick={() => setShow(show => !show)}>
+            <MenuIcon /><AccountCircleIcon className="user" />
+        </button>
+        {show && <Popup /> }
+            
+             
+             
+             
         </div>
     )
 }
---
+++ src/components/Popup.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Popup() {
+    return (
+        <div className="popup__connect">
+            <div>Inscription</div>
+            <div>Connection</div>
+            
+            <div>Héberger des voyageurs</div>
+            <div>Créer une expérience</div>
+            <div>Aide</div>
+        </div>
+    )
+}
+
+export default Popup
