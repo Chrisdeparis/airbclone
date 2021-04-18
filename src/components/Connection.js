bb37f56e41d506cec657e1fa81abcad7af43046e
maescristro <milfortchristian@gmail.com>
2021-04-18 16:08:03 +0200

    popup react-modal

--- src/components/Connection.js
+++ src/components/Connection.js
@@ -2,9 +2,10 @@ import React, {useState, useEffect} from 'react'
 import MenuIcon from '@material-ui/icons/Menu';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import Popup from './Popup';
+import Modal from 'react-modal';
 
 
-
+Modal.setAppElement('#root');
 
 function Connection() {
     
@@ -23,7 +24,33 @@ function Connection() {
         <button className="connect__btn" onClick={() => setShow(show => !show)}>
             <MenuIcon /><AccountCircleIcon className="user" />
         </button>
-            {show && <Popup /> }
+        <Modal className="popup__right" 
+            isOpen={show}
+            shouldCloseOnOverlayClick={true}
+            onRequestClose={() => setShow(false)}
+            style={
+                {
+                    overlay: {
+                        backgroundColor: 'transparent'
+                    },
+                    content: {
+                        position:'absolute',
+                        float:'right',
+                        right:'0',
+                        width:'300px',
+                        height:'250px',
+                        right:'40px',
+                        overflow:'none',
+                        inset:'0',
+                        backgroundColor:'transparent',
+                        border:'0',
+                        
+                    },
+                }
+            } >
+            <Popup />
+        </Modal>
+            
         </div>
     )
 }
