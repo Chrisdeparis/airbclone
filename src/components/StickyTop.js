2021a85722ef76faed9c8af800509a211feec8ef
maescristro <milfortchristian@gmail.com>
2021-04-18 19:25:27 +0200

    styled comp

--- src/components/StickyTop.js
+++ src/components/StickyTop.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react'
-import './StickyTop.css'
+
+import styled from 'styled-components';
 
 
 
@@ -8,7 +9,7 @@ function StickyTop() {
     const [stickyTop, setStickyTop] = useState(false);   
 
     const showStickyTop = () => {
-        //quand le scroll est en fin de page
+        //quand le scroll dépasse 60px
         let limit = 60;
         if(window.scrollY > limit){            
             const sticky = document.getElementById('stickyTop__nav');
@@ -30,21 +31,30 @@ function StickyTop() {
         let limit = 60;
         if(window.scrollY > limit) {
             setStickyTop(true);
-            console.log('affiche le sticky TOp');
         } else {
             setStickyTop(false);
-            console.log('masque le stickytop');
         }
 
     };
 
     window.addEventListener('scroll', hideStickyTop);
 
+    const NavSticky = styled.div`
+        position:fixed;
+        width:100%;
+        top:0;
+        height: 80px;
+        background:#fff!important;
+        color:#000;
+        z-index: 1000;
+        opacity:1!important;
+    `;
+
 
     return (
-        <div className={stickyTop ? 'stickyTop__nav' : ''} id="stickyTop__nav">
+        <NavSticky className={stickyTop ? 'stickyTop__nav' : ''} id="stickyTop__nav">
             <h1>Le Sticky Top</h1>
-        </div>
+        </NavSticky>
     )
 };
     
