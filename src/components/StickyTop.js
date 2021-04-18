488db5f20f486042999741f810444e8461720e52
maescristro <milfortchristian@gmail.com>
2021-04-19 00:45:19 +0200

    style comp icons

--- src/components/StickyTop.js
+++ src/components/StickyTop.js
@@ -5,6 +5,8 @@ import DevenezHote from './DevenezHote';
 import Langues from './Langues';
 import Connection from './Connection';
 
+import SearchIcon from '@material-ui/icons/Search';
+
 
 
 function StickyTop() {
@@ -43,25 +45,84 @@ function StickyTop() {
 
     const NavSticky = styled.div`
         position:fixed;
-        width:100%;
+        display:flex;
+
+        width:90%;
         top:0;
         height: 80px;
-        background:red;
-        color:#000;
+        background:#fff;
+        color:red;
         z-index: 1000;
         opacity:1!important;
+        padding:0 80px;
+        -webkit-box-shadow: 0 5px 4px -6px #767676;
+            -moz-box-shadow: 0 5px 4px -6px #767676;
+            box-shadow: 0 5px 4px -6px #767676;
     `;
     
     const Img = styled.img`
-        color:#f02f99;
+        object-fit: contain;
+        height: 40px;
+        margin-top: 20px;
+        
     `;
+    const InputSticky = styled.input`
+        width: 220px;
+        height: 30px;
+        border-radius: 50px;
+        outline: none;
+        border: 1px solid #dfdfdf;
+        text-align: center;
+        cursor: pointer;
+        font-weight: bold;
+        color: black;
+        font-size: 11px;
+        position:relative;
+        top:22px;
+        &::placeholder {
+            color:#222222;
+        }
+        &:hover {
+            -webkit-box-shadow: 0 5px 6px -6px #767676;
+            -moz-box-shadow: 0 5px 6px -6px #767676;
+            box-shadow: 0 5px 6px -6px #767676;
+        }
+    `;
+
+    const divIcon = styled.svg`
+        position: relative;
+        top: 26px;
+        height: 28px;
+        width: 28px;
+        border-radius: 50%;
+        background: #ff385c;
+    `;
+
+    const mysearchicon = styled.svg`
+        background: #ff385c;
+        width: 18px;
+        height: 18px;
+        position: relative;
+        top: 5px;
+        left: 5px;
+        color: #fff;
+        cursor: pointer;
+    `;
+
+    
+    
 
     
 
     return (
-        <NavSticky className={stickyTop ? 'stickyTop__nav' : ''} id="stickyTop__nav">
-            <Img className="sticky__icon" src="img/airbnb-svg.svg" alt=""/>
-              
+        <NavSticky id="stickyTop__nav">
+            <Img src="img/logo.svg" alt=""/>
+            <InputSticky type="text" placeholder="Commencer votre recherche" /> 
+            <divIcon>
+                <SearchIcon  />
+            </divIcon>
+                
+            
         </NavSticky>
     )
 };
