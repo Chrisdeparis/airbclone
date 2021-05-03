2e7457505f5f94dcb3e013b138970b93a20a80ff
maescristro <milfortchristian@gmail.com>
2021-05-03 22:37:33 +0200

    css modif

--- src/components/Localisation.js
+++ src/components/Localisation.js
@@ -4,7 +4,10 @@ import AppartGrid from './AppartGrid'
 function Localisation() {
     return (
         <div className="localisation">
+            <div>
             <a href="/" className="localisation__link">Airbnb</a> > <a className="localisation__link" href='/'>Logements</a> > France
+
+            </div>
             <h2 className="localisation__title">What guests are saying about hébergements in France</h2>
             <p className="localisation__p">Plus de <strong>21 000 000 évaluations de voyageurs</strong>, avec une moyenne de <strong>4.7 étoiles sur 5</strong></p>
             <AppartGrid />
